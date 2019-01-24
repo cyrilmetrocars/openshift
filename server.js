08a1b85b3507c627de3fffccd4f33b5733ea9336
cyrilmetrocars <46625121+cyrilmetrocars@users.noreply.github.com>
2019-01-24 19:57:43 +0100

    web server show views
    web server show ./views folder

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var express = require('express'),
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
+app.use(morgan('combined'));
+app.use(express.static('views'));
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
