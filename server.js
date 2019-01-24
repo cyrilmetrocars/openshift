d4492673d7b96fa908244769e4cdae5ec389bb19
cyrilmetrocars <46625121+cyrilmetrocars@users.noreply.github.com>
2019-01-24 20:29:26 +0100

    Server change

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'));
-app.use('/interface', express.static(__dirname + '/views'));
+app.use('/views', express.static(__dirname + '/views'));
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
@@ -74,7 +74,7 @@ var initDb = function(callback) {
   });
 };
 
-app.get('/', function (req, res) {
+app.get('/views/', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
@@ -88,10 +88,10 @@ app.get('/', function (req, res) {
       if (err) {
         console.log('Error running count. Message:\n'+err);
       }
-      res.render('readme.html', { pageCountMessage : count, dbInfo: dbDetails });
+      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
     });
   } else {
-    res.render('readme.html', { pageCountMessage : null});
+    res.render('index.html', { pageCountMessage : null});
   }
 });
 
