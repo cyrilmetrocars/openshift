af4696fc1aa4904479b0f6f764095ed004bcfd93
cyrilmetrocars <cyril.metrocars@gmail.com>
2019-01-25 08:32:10 +0100

    Update app_test.js

--- tests/app_test.js
+++ tests/app_test.js
@@ -9,6 +9,24 @@ reqServer = process.env.HTTP_TEST_SERVER || server
 
 describe('Basic routes tests', function() {
 
+    it('GET to / should return 200', function(done){
+        chai.request(reqServer)
+        .get('/')
+        .end(function(err, res) {
+            res.should.have.status(200);
+            done();
+        });
+    });
+
+    it('GET to /pagecount should return 200', function(done){
+        chai.request(reqServer)
+        .get('/pagecount')
+        .end(function(err, res) {
+            res.should.have.status(200);
+            done();
+        });
+    });
+    
     it('GET to /views should return 200', function(done){
         chai.request(reqServer)
         .get('/views/')
